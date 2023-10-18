4a9bebb446886578aa8baf3cc628114910148ffa
Alaa Mroue <alaa.baghdadi_mroue@stud.h2.de>
2023-10-18 13:47:24 +0200

    fork of Alaa's Promaides src 1e3ed7a8ccca28dcacd9fdcf8c5b37e9660b09d5

---
+++ source_code/system_hydraulic/source_code/models/groundwater/Hyd_Element_Groundwater.cpp
@@ -0,0 +1,11 @@
+#include "Hyd_Headers_Precompiled.h"
+#include "Hyd_Element_Groundwater.h"
+
+Hyd_Element_Groundwaterr::Hyd_Model_Groundwater()
+{
+}
+
+
+Hyd_Model_Groundwater::~Hyd_Model_Groundwater()
+{
+}
---
+++ source_code/system_hydraulic/source_code/models/groundwater/Hyd_Model_Groundwater.cpp
@@ -0,0 +1,12 @@
+#include "Hyd_Headers_Precompiled.h"
+#include "Hyd_Model_Groundwater.h"
+
+
+Hyd_Model_Groundwater::Hyd_Model_Groundwater()
+{
+}
+
+
+Hyd_Model_Groundwater::~Hyd_Model_Groundwater()
+{
+}
